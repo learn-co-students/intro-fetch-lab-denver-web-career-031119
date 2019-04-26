36c1a4bd9b17cda684346b562d9209b604e1234e
github username <github email address>
2019-04-26 14:17:01 -0600

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,68 @@
 // Write your numbers code in this file!
+const oneBtn = document.querySelector('#number-one')
+const oneFactsDiv = document.querySelector('#one-facts')
+const pickNum = document.querySelector('#pick-a-number')
+const randFact = document.querySelector('#random-math-fact')
+const yearHistory = document.querySelector('#year-history')
+const allNumBtn = document.querySelector('#all-numbers-button')
+const allNum = document.querySelector('#all-the-numbers')
+
+function fetchNumData (num, type) {
+  return fetch(`http://numbersapi.com/${num}/${type}`)
+  .then(response => response.text())
+}
+
+function oneClick () {
+  fetchNumData(1, 'trivia')
+    .then(data => {
+      oneFactsDiv.innerHTML = data
+    })
+}
+
+function randNumFact () {
+  let number = pickNum.value
+  if (isNaN(number)) {
+    randFact.innerHTML = "please enter a valid number"
+    } else {
+      fetchNumData(number, 'trivia')
+        .then(data => {
+          randFact.innerHTML = data
+        })
+    }
+}
+
+function yearFact (year) {
+  fetchNumData(year, 'year')
+    .then(data => {
+    yearHistory.innerHTML = data
+  })
+}
+
+function thisYear () {
+  let currentYear = 2019
+  yearFact(currentYear)
+  setInterval(function() {
+      currentYear -= 1
+      yearFact(currentYear)
+  }, 5000)
+}
+
+function allNumbers () {
+  return fetch(`http://numbersapi.com/1..100`)
+  .then(response => response.json())
+  .then(data => {
+    for (let key in data) {
+      console.log(data[key])
+      let ul = document.createElement('ul')
+      let li = document.createElement('li') 
+      li.innerHTML = `${data[key]}`
+      ul.appendChild(li)
+      allNum.appendChild(ul)
+    }
+  })
+}
+
+oneBtn.addEventListener('click', oneClick)
+allNumBtn.addEventListener('click', allNumbers)
+pickNum.addEventListener('change', randNumFact)
+document.addEventListener('DOMContentLoaded', thisYear())
--- swapi.js
+++ swapi.js
@@ -1,1 +1,51 @@
 // Write your swapi code in this file!
+let openingCrawlButton = document.querySelector('#crawlBtn')
+let planetInput = document.querySelector('#planetInput')
+let findPlanet = document.querySelector('#findPlanet')
+let planetData = document.querySelector('#planetData')
+let droid2 = document.querySelector('#droid-2')
+let droid3 = document.querySelector('#droid-3')
+
+
+function fetchData (type, num) {
+  return fetch(`https://cors-anywhere.herokuapp.com/https://swapi.co/api/${type}/${num}`, {header: {
+    "Access-Control-Allow-Origin": "*",
+    mode: "no-cors"
+  }})
+  .then(response => response.json())
+}
+
+function getOpeningCrawl () {
+  fetchData ('films', 1)
+    .then(data => {
+    crawlDiv = document.querySelector('#crawlDiv');
+    crawlDiv.innerHTML = data.opening_crawl;
+  });
+};
+
+function getPlanet () {
+  // planetInput.preventDefault()
+  let planetNumber = planetInput.value
+  fetchData('planets', planetNumber)
+    .then(data =>
+    planetData.innerHTML = `Name: ${data.name}, Climate: ${data.climate}`)
+}
+
+function getDroids () {
+  let droids = [2,3]
+  droids.map(function(d) {
+    fetchData('people', d)
+      .then(data => {
+        if (d === 2) {
+          droid2.innerHTML = `Name: ${data.name}, Height: ${data.height}, Mass: ${data.mass}`
+        } else {
+          droid3.innerHTML = `Name: ${data.name}, Height: ${data.height}, Mass: ${data.mass}`
+        }
+    })
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', getDroids ())
+openingCrawlButton.addEventListener('click', getOpeningCrawl)
+findPlanet.addEventListener('click', getPlanet)
